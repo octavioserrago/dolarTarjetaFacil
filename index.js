d73d6412e8cbc83ceccbc4d23995d4c782b54f0c
octavioserrago <octavioserrago.dev@gmail.com>
2024-03-08 01:06:30 -0300

    Improved UI, added consideration for additional taxes based on the selected region, and included a JSON file to store the IIBB tax rates for provinces.

--- index.js
+++ index.js
@@ -1,48 +1,89 @@
 import { obtenerPrecioDolarTarjeta } from './api.js';
 
-document.getElementById('calcularImpuestos').addEventListener('click', async function () {
-    const valorEnDolares = parseFloat(document.getElementById('valorCompra').value.trim());
-    const precioDolarOficial = await obtenerPrecioDolarTarjeta();
-    const valorPesosSinImpuestos = valorEnDolares * precioDolarOficial;
-    const valorIngresado = valorEnDolares * precioDolarOficial;
-
-    const resultadoImpuestoPaisElemento = document.getElementById('resultadoImpuestoPais');
-    const resultadoImpuestoGananciasElemento = document.getElementById('resultadoImpuestoGanancias');
-    const totalElemento = document.getElementById('total');
-
-    if (isNaN(valorIngresado) || valorIngresado <= 0) {
-        alert("Por favor, ingrese un valor válido mayor que cero.");
-    } else {
-        try {
-            const impuestoPais = (valorIngresado * 30) / 100;
-            const impuestoGanancias = (valorIngresado * 30) / 100;
-            const total = valorPesosSinImpuestos + impuestoPais + impuestoGanancias;
-
-            const impuestoPaisFormateado = impuestoPais.toFixed(2);
-            const impuestoGananciasFormateado = impuestoGanancias.toFixed(2);
-            const totalFormateado = total.toLocaleString('es-AR', {
-                style: 'currency',
-                currency: 'ARS'
+function cargarProvincias() {
+    fetch('iibbprovincias.json')
+        .then(response => response.json())
+        .then(data => {
+            const selectProvincia = document.getElementById('provincia');
+            data.provincias.forEach(provincia => {
+                const option = document.createElement('option');
+                option.value = provincia.nombre;
+                option.textContent = `${provincia.nombre} - ${provincia.iibb}% IIBB`;
+                selectProvincia.appendChild(option);
             });
 
-            resultadoImpuestoPaisElemento.innerText = `$${impuestoPaisFormateado}`;
-            resultadoImpuestoGananciasElemento.innerText = `$${impuestoGananciasFormateado}`;
-            totalElemento.innerText = totalFormateado;
+            document.getElementById('calcularImpuestos').addEventListener('click', async function () {
+                const valorEnDolares = parseFloat(document.getElementById('valorCompra').value.trim());
+                const precioDolarOficial = await obtenerPrecioDolarTarjeta();
+                const valorPesosSinImpuestos = valorEnDolares * precioDolarOficial;
+                const valorIngresado = valorEnDolares * precioDolarOficial;
 
-            document.getElementById('resultadosContainer').style.display = 'block';
-            document.getElementById('resultadoTotal').style.display = 'block';
+                const selectProvincia = document.getElementById('provincia');
+                const provinciaSeleccionada = selectProvincia.options[selectProvincia.selectedIndex].value;
 
-            if (window.innerWidth <= 768) {
-                const invitarBoton = document.getElementById('invitarBoton');
-                const computedStyles = getComputedStyle(invitarBoton);
-                const marginBottom = parseInt(computedStyles.marginBottom);
-                const newMarginBottom = marginBottom + 20;
-                invitarBoton.style.marginBottom = `${newMarginBottom}px`;
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
+                const resultadoImpuestoPaisElemento = document.getElementById('resultadoImpuestoPais');
+                const resultadoImpuestoGananciasElemento = document.getElementById('resultadoImpuestoGanancias');
+                const resultadoImpuestoIVAElemento = document.getElementById('resultadoImpuestoIVA');
+                const resultadoImpuestoIIBBElemento = document.getElementById('resultadoImpuestoIIBB');
+                const totalElemento = document.getElementById('total');
+
+                if (!provinciaSeleccionada) {
+                    alert("Por favor, selecciona una provincia.");
+                    return;
+                }
+
+                if (isNaN(valorIngresado) || valorIngresado <= 0) {
+                    alert("Por favor, ingresa un valor válido mayor que cero.");
+                    return;
+                }
+
+                try {
+                    const porcentajeIIBB = obtenerPorcentajeIIBB(data, provinciaSeleccionada);
+
+                    const impuestoPais = (valorIngresado * 8) / 100;
+                    const impuestoGanancias = (valorIngresado * 30) / 100;
+                    const impuestoIVA = (valorIngresado * 20.184) / 100;
+                    const impuestoIIBB = (valorIngresado * porcentajeIIBB) / 100;
+                    const total = valorPesosSinImpuestos + impuestoPais + impuestoGanancias + impuestoIVA + impuestoIIBB;
+
+                    const impuestoPaisFormateado = impuestoPais.toFixed(2);
+                    const impuestoGananciasFormateado = impuestoGanancias.toFixed(2);
+                    const impuestoIVAFormateado = impuestoIVA.toFixed(2);
+                    const impuestoIIBBFormateado = impuestoIIBB.toFixed(2);
+                    const totalFormateado = total.toLocaleString('es-AR', {
+                        style: 'currency',
+                        currency: 'ARS'
+                    });
+
+                    resultadoImpuestoPaisElemento.innerText = `$${impuestoPaisFormateado}`;
+                    resultadoImpuestoGananciasElemento.innerText = `$${impuestoGananciasFormateado}`;
+                    resultadoImpuestoIVAElemento.innerText = `$${impuestoIVAFormateado}`;
+                    resultadoImpuestoIIBBElemento.innerText = `$${impuestoIIBBFormateado}`;
+                    totalElemento.innerText = totalFormateado;
+
+                    document.getElementById('resultadosContainer').style.display = 'block';
+                    document.getElementById('resultadoTotal').style.display = 'block';
+
+                    if (window.innerWidth <= 768) {
+                        const invitarBoton = document.getElementById('invitarBoton');
+                        const computedStyles = getComputedStyle(invitarBoton);
+                        const marginBottom = parseInt(computedStyles.marginBottom);
+                        const newMarginBottom = marginBottom + 20;
+                        invitarBoton.style.marginBottom = `${newMarginBottom}px`;
+                    }
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            });
+        })
+        .catch(error => console.error('Error al cargar las provincias:', error));
+}
+
+function obtenerPorcentajeIIBB(data, provinciaSeleccionada) {
+    const provincia = data.provincias.find(provincia => provincia.nombre === provinciaSeleccionada);
+    return provincia ? provincia.iibb : 0;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    cargarProvincias();
 });
-
-
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# Calculadora de Compras al Exterior (USD)
+# Calculadora de Compras al Exterior (Dólar Tarjeta Simple)
 
-Este es un sitio web meramente informativo diseñado para calcular de forma rápida el costo total de las compras en el exterior, teniendo en cuenta los impuestos vigentes en Argentina.
+Este es un sitio web diseñado para calcular de manera rápida el costo total de las compras en el exterior, teniendo en cuenta los impuestos vigentes en Argentina, incluyendo el impuesto sobre los ingresos brutos (IIBB) aproximado de cada provincia, el Impuesto al Valor Agregado (IVA), el impuesto establecido por la Resolución General de AFIP 4815, y el Impuesto País.
 
 ## API Utilizada
 
@@ -9,13 +9,14 @@ Para obtener el precio de venta del dólar oficial, esta aplicación utiliza la 
 ## Cómo Usar
 
 1. Ingresa el valor de la compra en dólares en el campo proporcionado.
-2. Haz clic en el botón "Calcular Total" para obtener el costo total de la compra, incluyendo impuestos.
-3. Si has utilizado la aplicación, aparecerá un botón para invitarme a un cafecito.
+2. Selecciona la provincia desde la cual realizarás la compra para calcular el impuesto sobre los ingresos brutos (IIBB) aproximado.
+3. Haz clic en el botón "Calcular Total" para obtener el costo total de la compra, incluyendo los impuestos.
+4. Si deseas contribuir al desarrollo de esta herramienta, siéntete libre de hacerlo.
 
 ## Tecnologías Utilizadas
 
 - HTML
-- CSS (Tailwind CSS)
+- CSS (Tailwind CSS y CSS normal)
 - JavaScript
 
-
+¡Estén atentos para futuras actualizaciones y características adicionales! ¡Y si deseas contribuir, tu ayuda es bienvenida!
